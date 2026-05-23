c95d01f4064867eef97687513c5871bf8e8cb3bd
Konrad Bauer <konrad.bauer@remoterebels.com>
2026-05-23 21:37:44 +0200

    sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://studiocodeart.pl";
+const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://www.studiocodeart.pl";
 
 export default function robots(): MetadataRoute.Robots {
   return {
